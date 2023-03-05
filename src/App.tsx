d0493219b71e9de2328b56380f7afeafa264a346
huanniangstudio <891863891@qq.com>
2023-03-06 02:15:25 +0800

    Update demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,306 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import { Button, Card, Input, Radio } from "antd";
+var unisat = (window as any).unisat;
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const [publicKey, setPublicKey] = useState("");
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState({
+    confirmed: 0,
+    unconfirmed: 0,
+    total: 0,
+  });
+  const [network, setNetwork] = useState("livenet");
+
+  const getBasicInfo = async () => {
+    const address = await unisat.getAddress();
+    setAddress(address);
+
+    const publicKey = await unisat.getPublicKey();
+    setPublicKey(publicKey);
+
+    const balance = await unisat.getBalance();
+    setBalance(balance);
+
+    const network = await unisat.getNetwork();
+    setNetwork(network);
+  };
+
+  const selfRef = useRef<{ accounts: string[] }>({
+    accounts: [],
+  });
+  const self = selfRef.current;
+  const handleAccountsChanged = (_accounts: string[]) => {
+    if (self.accounts[0] === _accounts[0]) {
+      // prevent from triggering twice
+      return;
+    }
+    self.accounts = _accounts;
+    if (_accounts.length > 0) {
+      setAccounts(_accounts);
+      setConnected(true);
+
+      setAddress(_accounts[0]);
+
+      getBasicInfo();
+    } else {
+      setConnected(false);
+    }
+  };
+
+  const handleNetworkChanged = (network: string) => {
+    setNetwork(network);
+    getBasicInfo();
+  };
+
+  useEffect(() => {
+    if (!unisat) {
+      return;
+    }
+
+    unisat.on("accountsChanged", handleAccountsChanged);
+    unisat.on("networkChanged", handleNetworkChanged);
+
+    return () => {
+      unisat.removeListener("accountsChanged", handleAccountsChanged);
+      unisat.removeListener("networkChanged", handleNetworkChanged);
+    };
+  }, []);
+
+  if (!unisat) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <Button
+              onClick={() => {
+                window.location.href = "https://unisat.io";
+              }}
+            >
+              Install Unisat Wallet
+            </Button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Unisat Wallet Demo</p>
+
+        {connected ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Card
+              size="small"
+              title="Basic Info"
+              style={{ width: 300, margin: 10 }}
+            >
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>Address:</div>
+                <div style={{ wordWrap: "break-word" }}>{address}</div>
+              </div>
+
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>PublicKey:</div>
+                <div style={{ wordWrap: "break-word" }}>{publicKey}</div>
+              </div>
+
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>Balance: (Satoshis)</div>
+                <div style={{ wordWrap: "break-word" }}>{balance.total}</div>
+              </div>
+            </Card>
+
+            <Card
+              size="small"
+              title="Switch Network"
+              style={{ width: 300, margin: 10 }}
+            >
+              <div style={{ textAlign: "left", marginTop: 10 }}>
+                <div style={{ fontWeight: "bold" }}>Network:</div>
+                <Radio.Group
+                  onChange={async (e) => {
+                    const network = await unisat.switchNetwork(e.target.value);
+                    setNetwork(network);
+                  }}
+                  value={network}
+                >
+                  <Radio value={"livenet"}>livenet</Radio>
+                  <Radio value={"testnet"}>testnet</Radio>
+                </Radio.Group>
+              </div>
+            </Card>
+
+            <SignPsbtCard />
+            <SignMessageCard />
+            <PushTxCard />
+            <PushPsbtCard />
+          </div>
+        ) : (
+          <div>
+            <Button
+              onClick={async () => {
+                const result = await unisat.requestAccounts();
+                handleAccountsChanged(result);
+              }}
+            >
+              Connect Unisat Wallet
+            </Button>
+          </div>
+        )}
       </header>
     </div>
+  );
+}
+
+function SignPsbtCard() {
+  const [psbtHex, setPsbtHex] = useState("");
+  const [psbtResult, setPsbtResult] = useState("");
+  return (
+    <Card size="small" title="Sign Psbt" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>PsbtHex:</div>
+        <Input
+          defaultValue={psbtHex}
+          onChange={(e) => {
+            setPsbtHex(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Result:</div>
+        <div style={{ wordWrap: "break-word" }}>{psbtResult}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const psbtResult = await unisat.signPsbt(psbtHex);
+            setPsbtResult(psbtResult);
+          } catch (e) {
+            setPsbtResult((e as any).message);
+          }
+        }}
+      >
+        Sign Psbt
+      </Button>
+    </Card>
+  );
+}
+
+function SignMessageCard() {
+  const [message, setMessage] = useState("hello world~");
+  const [signature, setSignature] = useState("");
+  return (
+    <Card size="small" title="Sign Message" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Message:</div>
+        <Input
+          defaultValue={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Signature:</div>
+        <div style={{ wordWrap: "break-word" }}>{signature}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          const signature = await unisat.signMessage(message);
+          setSignature(signature);
+        }}
+      >
+        Sign Message
+      </Button>
+    </Card>
+  );
+}
+
+function PushTxCard() {
+  const [rawtx, setRawtx] = useState("");
+  const [txid, setTxid] = useState("");
+  return (
+    <Card
+      size="small"
+      title="Push Transaction Hex"
+      style={{ width: 300, margin: 10 }}
+    >
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>rawtx:</div>
+        <Input
+          defaultValue={rawtx}
+          onChange={(e) => {
+            setRawtx(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>txid:</div>
+        <div style={{ wordWrap: "break-word" }}>{txid}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const txid = await unisat.pushTx(rawtx);
+            setTxid(txid);
+          } catch (e) {
+            setTxid((e as any).message);
+          }
+        }}
+      >
+        PushTx
+      </Button>
+    </Card>
+  );
+}
+
+function PushPsbtCard() {
+  const [psbtHex, setPsbtHex] = useState("");
+  const [txid, setTxid] = useState("");
+  return (
+    <Card size="small" title="Push Psbt Hex" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>psbt hex:</div>
+        <Input
+          defaultValue={psbtHex}
+          onChange={(e) => {
+            setPsbtHex(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>txid:</div>
+        <div style={{ wordWrap: "break-word" }}>{txid}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const txid = await unisat.pushPsbt(psbtHex);
+            setTxid(txid);
+          } catch (e) {
+            setTxid((e as any).message);
+          }
+        }}
+      >
+        pushPsbt
+      </Button>
+    </Card>
   );
 }
 

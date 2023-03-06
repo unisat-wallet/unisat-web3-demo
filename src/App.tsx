318e628ed003c01e976150ef34894b1c478bdff8
huanniangstudio <891863891@qq.com>
2023-03-06 16:16:20 +0800

    Add SendBitcoin

--- src/App.tsx
+++ src/App.tsx
@@ -150,6 +150,7 @@ function App() {
             <SignMessageCard />
             <PushTxCard />
             <PushPsbtCard />
+            <SendBitcoin />
           </div>
         ) : (
           <div>
@@ -303,6 +304,54 @@ function PushPsbtCard() {
         }}
       >
         pushPsbt
+      </Button>
+    </Card>
+  );
+}
+
+function SendBitcoin() {
+  const [toAddress, setToAddress] = useState(
+    "tb1qmfla5j7cpdvmswtruldgvjvk87yrflrfsf6hh0"
+  );
+  const [satoshis, setSatoshis] = useState(1000);
+  const [txid, setTxid] = useState("");
+  return (
+    <Card size="small" title="Send Bitcoin" style={{ width: 300, margin: 10 }}>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Receiver Address:</div>
+        <Input
+          defaultValue={toAddress}
+          onChange={(e) => {
+            setToAddress(e.target.value);
+          }}
+        ></Input>
+      </div>
+
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>Amount: (satoshis)</div>
+        <Input
+          defaultValue={satoshis}
+          onChange={(e) => {
+            setSatoshis(parseInt(e.target.value));
+          }}
+        ></Input>
+      </div>
+      <div style={{ textAlign: "left", marginTop: 10 }}>
+        <div style={{ fontWeight: "bold" }}>txid:</div>
+        <div style={{ wordWrap: "break-word" }}>{txid}</div>
+      </div>
+      <Button
+        style={{ marginTop: 10 }}
+        onClick={async () => {
+          try {
+            const txid = await unisat.sendBitcoin(toAddress, satoshis);
+            setTxid(txid);
+          } catch (e) {
+            setTxid((e as any).message);
+          }
+        }}
+      >
+        SendBitcoin
       </Button>
     </Card>
   );
